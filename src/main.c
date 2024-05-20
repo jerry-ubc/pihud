c4cfc4dbea9c02897e07b7eb62eeee232f761c4b
jerry-ubc <jerry.jge@gmail.com>
2024-05-20 15:50:19 +0800

    added OLED changes

--- src/main.c
+++ src/main.c
@@ -7,9 +7,12 @@
 #include <linux/i2c-dev.h>
 #include <sys/ioctl.h>
 #include <time.h>
-
+#include <errno.h>
 #include "led.h"
 #include "config.h"
+
+#define I2C_DEVICE "/dev/i2c-1"  // Check your Pi version, might be i2c-0
+#define OLED_ADDR  0x3C         // Typically 0x3C or 0x3D
 
 //Function declarations
 void initialize_components();
@@ -17,6 +20,9 @@ void display_startup_message();
 void update_display();
 void speed_alert();
 void delay(int milliseconds);
+
+//Globals
+int i2c_fd; //TODO: should we make this an extern in config.h?
 
 int main() {
     initialize_components();
@@ -32,12 +38,17 @@ int main() {
 }
 
 void initialize_components() {
+    if ((i2c_fd = open(I2C_DEVICE, O_RDWR)) < 0) {
+        perror("Failed to open the i2c bus");
+        exit(1);
+    }
+    if (ioctl(i2c_fd, I2C_SLAVE, OLED_ADDR) < 0) {
+        perror("Failed to communicate with the OLED device");
+        exit(1);
+    }
     return;
 }
 void display_startup_message() {
-    return;
-}
-void update_display() {
     return;
 }
 void speed_alert() {
@@ -50,4 +61,16 @@ void delay(int milliseconds) {
     ts.tv_nsec = (milliseconds % 1000) * 1000000L;  //remainder to nanoseconds
 
     nanosleep(&ts, NULL);
+}
+
+void write_i2c(int command) {
+    if (write(i2c_fd, &command, 1) != 1) {
+        perror("Failed to write to the i2c device");
+    }
+}
+
+void update_display() {
+    // Commands to control what to display
+    // For instance, clearing the display, setting cursor position, etc.
+    write_i2c(0x00);  // Example command
 }
