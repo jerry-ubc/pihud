42bd13f8ce23d1d4e369f8f479d4406ffa97aaf9
jerry-ubc <jerry.jge@gmail.com>
2024-06-03 22:52:07 +0800

    added can-isotp, but possibly unnecessary

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+//TODO: remove can-isotp folder and its related git stuff
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,18 +15,18 @@
 #include "config.h"
 
 ssd1306_i2c_t *oled = NULL;
-ssd1306_framebuffer_t *fbp_keys = NULL;  //TODO: should these be extern or put in config?
-ssd1306_framebuffer_t *fbp_values = NULL;
+ssd1306_framebuffer_t *fbp = NULL;  //TODO: should these be extern or put in config?
 
-#define I2C_DEVICE "/dev/i2c-1"  // Check your Pi version, might be i2c-0
-#define OLED_ADDR  0x3C         // Typically 0x3C or 0x3D
+#define I2C_DEVICE "/dev/i2c-1"
+#define OLED_ADDR  0x3C
 
 //Function declarations
-void update_display(const char *text);
-void delay(int milliseconds);
 int init_display();
+void update_display(const char *text);
+void update_values(int speed, int throttle);
+
 void cleanup_display();
-void update_values(int speed, int engine_speed);
+void delay(int milliseconds);
 
 //Globals
 int i2c_fd; //TODO: should we make this an extern in config.h?
@@ -34,52 +35,44 @@ ssd1306_framebuffer_box_t bbox;
 
 int main() {
     init_display();
-
     while (1) {
-        // update_display("1234567891234\n1234567891");
         sleep(1);
-        update_values(888, 77);
+        update_values(888, 70);
         sleep(1);
     }
-
-    // ssd1306_framebuffer_draw_line(fbp, 0, 0, 64, 32, true);
-    // ssd1306_framebuffer_draw_line(fbp, 64, 31, 128, 31, true);
-    // ssd1306_framebuffer_draw_line(fbp, 0, 0, 0, 32, true);
-    // ssd1306_framebuffer_bitdump(fbp);
-    // ssd1306_i2c_display_update(oled, fbp);
     return 0;
 }
 
-
 void update_display(const char *text) {
-    if (!oled || !fbp_keys) {
+    if (!oled || !fbp) {
         fprintf(stderr, "ERROR: Display not initialized.\n");
         return;
     }
     //TODO: check max length of text
-    ssd1306_framebuffer_clear(fbp_keys);
-    ssd1306_framebuffer_draw_text(fbp_keys, text, 0, 0, 16, SSD1306_FONT_DEFAULT, 5, &bbox);
-    ssd1306_i2c_display_update(oled, fbp_keys);
+    ssd1306_framebuffer_clear(fbp);
+    ssd1306_framebuffer_draw_text(fbp, text, 0, 0, 16, SSD1306_FONT_DEFAULT, 5, &bbox);
+    ssd1306_i2c_display_update(oled, fbp);
 }
 
-void update_values(int speed, int engine_speed) {
-    if (!oled || !fbp_keys) {
+void update_values(int speed, int throttle) {
+    if (!oled || !fbp) {
         fprintf(stderr, "ERROR: Display not initialized.\n");
         return;
     }
     char speed_str[3]; //concat string to 3 digits
     sprintf(speed_str, "%d", speed);
-    char engine_speed_str[3];
-    sprintf(engine_speed_str, "%dk", engine_speed);
+    char throttle_str[4];
+    sprintf(throttle_str, "%d%%", throttle);
 
-    // ssd1306_framebuffer_clear(fbp_values);
+    ssd1306_framebuffer_draw_text(fbp, "Speed", 0, 0, 12, SSD1306_FONT_DEFAULT, 4, &bbox);
+    ssd1306_framebuffer_draw_text(fbp, "Throttle", 0, 0, 28, SSD1306_FONT_DEFAULT, 4, &bbox);
+    ssd1306_framebuffer_draw_text(fbp, speed_str, 0, 96, 12, SSD1306_FONT_DEFAULT, 4, &bbox);
+    if (throttle < 100)
+        ssd1306_framebuffer_draw_text(fbp, throttle_str, 0, 90, 28, SSD1306_FONT_DEFAULT, 4, &bbox);
+    else
+        ssd1306_framebuffer_draw_text(fbp, throttle_str, 0, 79, 28, SSD1306_FONT_DEFAULT, 4, &bbox);
 
-    ssd1306_framebuffer_draw_text(fbp_keys, "Speed:", 0, 0, 12, SSD1306_FONT_DEFAULT, 4, &bbox);
-    ssd1306_framebuffer_draw_text(fbp_keys, "eSpeed:", 0, 0, 28, SSD1306_FONT_DEFAULT, 4, &bbox);
-    ssd1306_framebuffer_draw_text(fbp_keys, speed_str, 0, 90, 12, SSD1306_FONT_DEFAULT, 4, &bbox);
-    ssd1306_framebuffer_draw_text(fbp_keys, engine_speed_str, 0, 90, 28, SSD1306_FONT_DEFAULT, 4, &bbox);
-
-    ssd1306_i2c_display_update(oled, fbp_keys);
+    ssd1306_i2c_display_update(oled, fbp);
 }
 
 int init_display() {
@@ -94,25 +87,18 @@ int init_display() {
         ssd1306_i2c_close(oled);
         return -1;
     }
-    fbp_keys = ssd1306_framebuffer_create(oled->width, oled->height, oled->err);
-    if (!fbp_keys) {
+    fbp = ssd1306_framebuffer_create(oled->width, oled->height, oled->err);
+    if (!fbp) {
         fprintf(stderr, "ERROR: Failed to create framebuffer for keys.\n");
         ssd1306_i2c_close(oled);
         return -1;
     }
-    fbp_values = ssd1306_framebuffer_create(oled->width, oled->height, oled->err);
-    if (!fbp_values) {
-        fprintf(stderr, "ERROR: Failed to create framebuffer for values.\n");
-        ssd1306_i2c_close(oled);
-        return -1;
-    }
     ssd1306_i2c_display_clear(oled);
-    ssd1306_framebuffer_clear(fbp_keys);
-    ssd1306_framebuffer_clear(fbp_values);
+    ssd1306_framebuffer_clear(fbp);
 
-    ssd1306_framebuffer_draw_text(fbp_keys, "Speed:", 0, 0, 12, SSD1306_FONT_DEFAULT, 4, &bbox);
-    ssd1306_framebuffer_draw_text(fbp_keys, "eSpeed:", 0, 0, 28, SSD1306_FONT_DEFAULT, 4, &bbox);
-    ssd1306_i2c_display_update(oled, fbp_keys);
+    ssd1306_framebuffer_draw_text(fbp, "Speed", 0, 0, 12, SSD1306_FONT_DEFAULT, 4, &bbox);
+    ssd1306_framebuffer_draw_text(fbp, "Throttle", 0, 0, 28, SSD1306_FONT_DEFAULT, 4, &bbox);
+    ssd1306_i2c_display_update(oled, fbp);
     return 0;
 }
 
